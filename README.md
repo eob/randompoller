ec0b1fd40d84ceacf993a3644f21aa57c62aabf4
Edward Benson <edward.benson@gmail.com>
2013-03-16 18:10:02 -0400

    initial setup

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ randompoller
 ============
 
 Playing with the Component framework..
+
+To Run
+------
+
+$ component install eob/randompoller
+
+Then
+
+   
+   var RandomPoller = require('randompoller');
+   var rp = new RandomPoller();
+   rp.on('BANG', function() {
+     alert("Fire!");
+   });
---
+++ index.js
@@ -0,0 +1,47 @@
+
+/**
+ * Module dependencies.
+ */
+
+var emitter = require('emitter');
+
+/**
+ * Expose `RandomPoller`.
+ */
+
+module.exports = RandomPoller;
+
+/**
+ * Initialize a new `RandomPoller`.
+ *
+ * @api public
+ */
+
+function RandomPoller() {
+
+  // Mix the Emitter functionality into the Random Poller
+  // This enables calles to listen to us with 
+  // var rp = new RandomPoller();
+  // rp.on('eventName', function() {...})
+  for (var key in Emitter.prototype) {
+    this.key = Emitter.prototype.key;
+  }
+
+  // Now we start emitting random events
+  this.startEmittingRandomEvents();
+  
+};
+
+RandomPoller.prototype.startEmittingRandomEvents = function() {
+  var seconds = 5000 * Math.random(); // [0, 5] seconds
+  this.timer_ = window.setInterval(this.eventTimerFired, seconds);
+};
+
+RandomPoller.prototype.eventTimerFired = function() {
+  this.emit('BANG');
+};
+
+RandomPoller.prototype.stopEmittingRandomEvents = function() {
+  window.clearInterval(this.timer_);
+  this.timer_ = null;
+};
